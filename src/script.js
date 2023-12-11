40245b0a8ee1a2f3dbe50a782dbe1cac9c6dd245
Ameer <ameerapex496@gmail.com>
2023-12-11 04:36:14 +0100

    added comment , selected elements + added eventlinster

--- src/script.js
+++ src/script.js
@@ -1,1 +1,9 @@
-console.log("hallo world");
+// selecting our elements
+var passwordInput = document.getElementsByClassName("password-Input")[0];
+// our global veribales
+// our functions
+// our eventlinsters
+passwordInput.addEventListener("input", function (e) {
+    var valInput = e.target.value;
+    console.log(valInput);
+});
--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,12 @@
-console.log("hallo world");
+// selecting our elements
+const passwordInput = document.getElementsByClassName("password-Input")[0];
+
+// our global veribales
+
+// our functions
+
+// our eventlinsters
+passwordInput.addEventListener("input", (e: any) => {
+  let valInput = e.target.value;
+  console.log(valInput);
+});
