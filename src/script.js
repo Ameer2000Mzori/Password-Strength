d545a736d123be351d155dbc2f77efb1cac31e5c
Ameer <ameerapex496@gmail.com>
2023-12-11 04:50:06 +0100

    added styling to background image /header

--- src/script.js
+++ src/script.js
@@ -1,9 +1,16 @@
 // selecting our elements
 var passwordInput = document.getElementsByClassName("password-Input")[0];
+var headerEl = document.getElementsByClassName("header")[0];
 // our global veribales
 // our functions
-// our eventlinsters
-passwordInput.addEventListener("input", function (e) {
-    var valInput = e.target.value;
-    console.log(valInput);
-});
+// const changeBackGroundColor = (valInput) => {
+//   let lengthCount = valInput.length;
+//   console.log(lengthCount);
+//   headerEl.style.filter = `blur(${lengthCount * 10}-px)`;
+// };
+// // our eventlinsters
+// passwordInput.addEventListener("input", (e: any) => {
+//   let valInput = e.target.value;
+//   changeBackGroundColor(valInput);
+// });
+// changeBackGroundColor(50);
--- src/script.ts
+++ src/script.ts
@@ -1,12 +1,20 @@
 // selecting our elements
 const passwordInput = document.getElementsByClassName("password-Input")[0];
+const headerEl: any = document.getElementsByClassName("header")[0];
 
 // our global veribales
 
 // our functions
+// const changeBackGroundColor = (valInput) => {
+//   let lengthCount = valInput.length;
+//   console.log(lengthCount);
+//   headerEl.style.filter = `blur(${lengthCount * 10}-px)`;
+// };
 
-// our eventlinsters
-passwordInput.addEventListener("input", (e: any) => {
-  let valInput = e.target.value;
-  console.log(valInput);
-});
+// // our eventlinsters
+// passwordInput.addEventListener("input", (e: any) => {
+//   let valInput = e.target.value;
+//   changeBackGroundColor(valInput);
+// });
+
+// changeBackGroundColor(50);
